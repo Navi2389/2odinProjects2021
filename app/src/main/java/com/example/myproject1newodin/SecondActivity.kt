865a54d2e53300ef0916b869431e5474fdd38b85
KolupaevIvan <koluivan@yandex.ru>
2021-12-05 19:36:47 +0300

    Попытка 6 или7 Комит для реквест пула. Самостоятельная работа №6.Используя документацию Android, изучить возможности Intent. Открыть браузер с помощью вызова Intent.

---
+++ app/src/main/java/com/example/myproject1newodin/SecondActivity.kt
@@ -0,0 +1,15 @@
+package com.example.myproject1newodin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+    }
+}
+/******/
+/******/
+/******/
